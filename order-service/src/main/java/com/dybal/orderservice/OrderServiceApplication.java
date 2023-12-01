12a502fc0e3b192c492eb93db1906fbf12b04429
klaudia <klaudia.dybal@gmail.com>
2023-12-01 07:02:32 +0100

    Updating modules names.

--- order-service/src/main/java/com/dybal/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/dybal/orderservice/OrderServiceApplication.java
@@ -1,13 +1,13 @@
-package com.dybal.ordersservice;
+package com.dybal.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrdersServiceApplication {
+public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrdersServiceApplication.class, args);
+		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
 }
--- order-service/src/test/java/com/dybal/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/dybal/orderservice/OrderServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dybal.ordersservice;
+package com.dybal.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrdersServiceApplicationTests {
+class OrderServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
